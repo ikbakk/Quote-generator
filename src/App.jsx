76bd52d6302b870bfbf94c4495363f9f27d1c3b9
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-18 11:27:58 +0800

    fetch data when clicking button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { useState, useEffect } from 'react'
 
 const App = () => {
-  const [data, setData] = useState('')
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+  const [err, setErr] = useState(null)
 
-  useEffect(() => {
-    fetch(`https://api.api-ninjas.com/v1/quotes?category=humor`, {
-      method: 'GET',
-      headers: {
-        'X-Api-Key': import.meta.env.VITE_API_KEY
+  const handleClick = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(
+        'https://api.api-ninjas.com/v1/quotes?category=humor',
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'X-Api-Key': import.meta.env.VITE_API_KEY
+          }
+        }
+      )
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`)
       }
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err))
-  }, [])
+      const result = await response.json()
+      setData(result)
+    } catch (err) {
+      setErr(err.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
-  return <>tes</>
+  return (
+    <>
+      <button onClick={handleClick}>Click</button>
+    </>
+  )
 }
 
 export default App
